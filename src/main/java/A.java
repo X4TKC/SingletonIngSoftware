648b7804fe1ba02e62a7fe18bcff4add6ae41ee0
x4tkc <tovilaskevin@gmail.com>
2018-04-20 07:48:49 -0400

    Dev

---
+++ src/main/java/A.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Asus on 4/20/2018.
+ */
+public class A {
+
+    private String Alpha = "Asd";
+
+}
---
+++ src/main/java/B.java
@@ -0,0 +1,6 @@
+/**
+ * Created by Asus on 4/20/2018.
+ */
+public class B {
+    private String Beta = "Kevin";
+}
